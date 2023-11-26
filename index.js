c464b96723f86e6b11036ef8fcecf672edeba335
GuilleCOzzi <gecozzi87@gmail.com>
2023-11-25 22:47:07 -0300

    appV 0.2

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-const keyAPI = "69b51fd13e96589445a76e1ac7671b2a";
-var valorTemp = document.getElementById(temp-valor);
-var tempDesc = document.getElementById(temp-desc);
+const keyAPI = "";
+
+var valorTemp = document.getElementById(tempValor);
+var tempDesc = document.getElementById(tempDesc);
 var ubi = document.getElementById(ubication);
-var icono = document.getElementById(inimation);
-var velocidadViento = document.getElementById(wind-speed);
-window.addEventListener('load', () =>{
+var icono = document.getElementById(animation);
+var velocidadViento = document.getElementById(windSpeed);
+
+window.addEventListener('load', ()=>{
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(posicion => {
-            var latitude = posicion.coords.latitude;
-            var longitude = posicion.coords.longitude;
+            console.log(posicion)
+            var lat = posicion.coords.latitude;
+            var long = posicion.coords.longitude;
 
-            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}$&lon=${longitude}&appid=${keyAPI}`;
-            const city = `https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}`
-        });
+            const urlapi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}$&lon=${long}&appid=69b51fd13e96589445a76e1ac7671b2a`;
+            //const city = `https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}`
+
+            fetch(urlapi)
+            .then(Response =>{return Response.json()})
+            .then(datosClima =>{
+                console.log(datosClima)
+                var temperature = Math.round(datosClima.main.temp)
+                valorTemp.textContent = `${temperature} °C`
+                var desc = datosClima.weather[0].description;
+                tempDesc.textContent = desc.toUpperCase();
+                ubi.textContent = datosClima.name;
+                velocidadViento.textContent = `${datosClima.wind.speed} M/S`
+
+            })
+
+        })
     }
-});
+})
